8b04a492ed083b6837d6c18af1b56a7d07941c7b
André Furlan <ensismoebius@gmail.com>
2020-08-17 21:52:04 -0300

    Melhoria no script para windows

--- build/setupWindowsVisualStudio.bat
+++ build/setupWindowsVisualStudio.bat
@@ -1,8 +1,5 @@
 @echo off
 
 conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
-conan install ..
-cmake .. -G "Visual Studio 16"
-REM cmake --build . --config Release
-cmake --build . --config Debug
+conan install .. -s build_type=Debug 
+REM conan install .. -s build_type=Release
