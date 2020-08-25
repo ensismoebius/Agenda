b00570d6b73834a41ba9021efb7b75378bad4110
Andre Furlan <ensismoebius@gmail.com>
2020-08-25 00:29:24 -0300

    Uma remodelada no sistema de build

--- build/setupGNULinuxGcc.sh
+++ build/setupGNULinuxGcc.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
+
+###############
+### Options ###
+###############
+
+#####################
+## Choose your IDE ##
+#####################
+
+#ide="Unix Makefiles"
+#ide="Green Hills MULTI"
+#ide="Ninja"
+#ide="Watcom WMake"
+#ide="CodeBlocks - Ninja"
+#ide="CodeBlocks - Unix Makefiles"
+#ide="CodeLite - Ninja"
+#ide="CodeLite - Unix Makefiles"
+#ide="Sublime Text 2 - Ninja"
+#ide="Sublime Text 2 - Unix Makefiles"
+#ide="Kate - Ninja"
+#ide="Kate - Unix Makefiles"
+#ide="Eclipse CDT4 - Ninja"
+ide="Eclipse CDT4 - Unix Makefiles"
+
+############################
+## Release or Debug mode? ##
+############################
+dr="Debug"
+#dr="Release"
+
+###############################################
+### Running the build and dependency solver ###
+###############################################
+
+##############################
+## Running depedency solver ##
+##############################
+
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
-conan install ..
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
-#cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+conan install .
+
+#######################
+## Building software ##
+#######################
+cmake . -G "$ide" -DCMAKE_BUILD_TYPE=$dr
+cmake --build . 
